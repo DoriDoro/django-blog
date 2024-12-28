de5e0e1cf5d61b2a1f0f8bd749975d6c970ac9f9
doridoro <dorothea.reher@gmail.com>
2024-12-28 12:19:23 +0100

    add gunicorn and whitenoise - start making production ready

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -98,6 +99,16 @@ DATABASES = {
         "HOST": config("DB_HOST", "localhost"),
         "PORT": "5432",
     }
+}
+
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
 }
 
 
