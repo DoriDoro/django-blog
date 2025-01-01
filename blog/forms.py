2eb35a07069217c2d8446e1da32f2146c9e0d284
doridoro <dorothea.reher@gmail.com>
2025-01-01 12:11:54 +0100

    modified submit btns, label of EmailPostForm

--- blog/forms.py
+++ blog/forms.py
@@ -11,24 +11,45 @@ class EmailPostForm(forms.Form):
     name = forms.CharField(
         max_length=25,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Your name"}
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your name",
+                "aria-label": "Your name",
+            }
         ),
+        label="",
     )
     email = forms.EmailField(
         widget=forms.EmailInput(
-            attrs={"class": "form-control", "placeholder": "Your Email address"}
-        )
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your Email Address",
+                "aria-label": "Your Email Address",
+            }
+        ),
+        label="Your Email Address",
     )
     to = forms.EmailField(
         widget=forms.EmailInput(
-            attrs={"class": "form-control", "placeholder": "Email address of receiver"}
-        )
+            attrs={
+                "class": "form-control",
+                "placeholder": "Email Address of receiver",
+                "aria-label": "Email Address of receiver",
+            }
+        ),
+        label="Receiver Email Address",
     )
     comments = forms.CharField(
         required=False,
         widget=forms.Textarea(
-            attrs={"class": "form-control", "rows": 6, "placeholder": "Your message"}
+            attrs={
+                "class": "form-control",
+                "rows": 6,
+                "placeholder": "Your message ...",
+                "aria-label": "Your message ...",
+            }
         ),
+        label="",
     )
 
 
