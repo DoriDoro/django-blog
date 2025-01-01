3d97756d4bfbaeed8ef77b8b50b185389600b79f
doridoro <dorothea.reher@gmail.com>
2025-01-01 16:26:49 +0100

    changed the general settings

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -23,16 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True").lower() == "false"
+DEBUG = config("DEBUG", "True").lower() == "false"
 
-ALLOWED_HOSTS = (
-    os.environ.get("ALLOWED_HOSTS").split(",")
-    if os.environ.get("ALLOWED_HOSTS")
-    else []
-)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
 
 
 # Application definition
